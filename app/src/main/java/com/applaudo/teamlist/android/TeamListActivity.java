3337b054ca385ae4a8b9f877d051f244a72951e9
Luis Ventura <luis.ventura@outlook.com>
2017-10-27 02:52:00 -0600

    initial release, template app with empty activity

---
+++ app/src/main/java/com/applaudo/teamlist/android/TeamListActivity.java
@@ -0,0 +1,13 @@
+package com.applaudo.teamlist.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeamListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_list);
+    }
+}
