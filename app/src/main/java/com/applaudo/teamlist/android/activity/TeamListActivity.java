b8612f12e9b8f4dff44d72914d09d838e1ce8eab
Luis Ventura <luis.ventura@outlook.com>
2017-10-28 01:48:19 -0600

    added method for api call through singleton

--- app/src/main/java/com/applaudo/teamlist/android/activity/TeamListActivity.java
+++ app/src/main/java/com/applaudo/teamlist/android/activity/TeamListActivity.java
@@ -3,7 +3,22 @@ package com.applaudo.teamlist.android.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.applaudo.teamlist.android.model.Team;
+import com.applaudo.teamlist.android.network.restApi;
+
 import com.applaudo.teamlist.android.R;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TeamListActivity extends Activity {
 
@@ -12,4 +27,31 @@ public class TeamListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_list);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        requestTeams();
+
+    }
+
+    private void requestTeams(){
+
+        Call<List<Team>> teams = restApi.getInstance().APICall().getTeamList();
+        teams.enqueue(new Callback<List<Team>>() {
+            @Override
+            public void onResponse(Call<List<Team>> call, Response<List<Team>> response) {
+                Toast.makeText(TeamListActivity.this, "New Teams have been received!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Team>> call, Throwable t) {
+                Toast.makeText(TeamListActivity.this, "Failed to refresh! Is your Internet on?", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
 }
